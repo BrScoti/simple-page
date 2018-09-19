9da3a52ed40f07dd9017b3e7d557b551a10281c1
Bruno Scoti <bruno.scoti@yahoo.com.br>
2018-09-19 09:43:55 -0300

    Segundo commit, adição, e listagem funcionando

--- client/main.js
+++ client/main.js
@@ -3,10 +3,10 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import './main.html';
-
+import { Fornecedores } from '/lib/colections/main.js'
+//Main
 Template.main.onCreated(function () {
   this.status = new ReactiveVar('list');
-  console.log('ii');
 });
 
 Template.main.events({
@@ -20,28 +20,81 @@ Template.main.events({
 
   },
   'click #save'(event, instance) {
-    let name, email, quant;
-    name= document.getElementById('inputName').value;
-    email= document.getElementById('inputEmail').value;
-    quant= document.getElementById('inputQuant').value;
-     alert(`${name}${email}${quant}`);
+    let nameInput = document.getElementById('inputName').value;
+    let emailInput = document.getElementById('inputEmail').value;
+    let quantInput = document.getElementById('inputQuant').value;
+
+    Fornecedores.insert({
+      name: nameInput,
+      email: emailInput,
+      quant: quantInput
+    });
+    nameInput = '';
+    emailInput = '';
+    quantInput = '';
+    instance.status.set('list');
   }
 });
 Template.main.helpers({
   tela(pg) {
     return Template.instance().status.get() === pg;
   }
+
 })
 
-
+//Add
 Template.add.onCreated(function addOnCreated() {
-
+  this.fornecedor = new ReactiveVar({});
 
 });
+Template.add.helpers({
+  fornecedor() {
+    return Template.instance().status.get();
+  }
+});
+
 Template.add.events({
-  
-})
+
+});
+
+//List
 Template.list.onCreated(function addOnCreated() {
 
 });
+Template.list.helpers({
+  fornecedores() {
+    return Fornecedores.find().fetch();
+  }
+})
+Template.list.events({
+  'click #edit'(event, instance) {
+    Blaze.renderWithData(Template['edit'], this, document.body);
 
+
+  }
+})
+//Edit
+Template.edit.onCreated(function editOnCreated() {
+  this.fornecedor = new ReactiveVar();
+  this.fornecedor.set(this.data);
+
+
+
+});
+Template.edit.helpers({
+  fornecedor() {
+    return Template.instance().fornecedor.get();
+  }
+})
+Template.edit.onRendered(function editOnRendered() {
+
+});
+
+Template.edit.events({
+  'click #close'(event, instance) {
+    //Blaze.remove(instance.view);
+  },
+  'click #save'(event, instance) {
+
+  }
+});
